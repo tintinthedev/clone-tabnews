6cadd48ebb8f475524f203297d71df2a2685f64a
Timoteo <randomthecoder1557@gmail.com>
2024-11-23 10:07:24 -0800

    modifica pagina inicial com a imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Image from "next/image"
+
 function Home() {
-  return <h1>Tome mofi tome!</h1>
+  return (
+    <>
+      <h1>Tome mofi tome</h1>
+      <Image
+        src={"/raleumofi.png"}
+        alt="Imagem de agradecimento para um colega"
+        width={100}
+        height={100}
+      />
+    </>
+  )
 }
 
 export default Home;
