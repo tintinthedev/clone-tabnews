4fc4a89324ba5de96a0c771c89d0debc369f8765
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-11-25 11:44:05 +0000

    adiciona e executa scritps `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 
 function Home() {
   return (
@@ -11,7 +11,7 @@ function Home() {
         height={100}
       />
     </>
-  )
+  );
 }
 
 export default Home;
