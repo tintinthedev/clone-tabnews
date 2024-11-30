4cbaf7054ec4e536f432e797180f354743fb2662
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-11-30 21:17:46 +0000

    finish `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).not.toBeNull();
+
+  const parsedBodyUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(parsedBodyUpdatedAt).toEqual(responseBody.updated_at);
+
+  const { version, max_connections, opened_connections } =
+    responseBody.dependencies.database;
+  expect(version).toBe("16.0");
+
+  expect(max_connections).toBeDefined();
+  expect(Number.isInteger(max_connections)).toBe(true);
+  expect(max_connections).toBeGreaterThanOrEqual(0);
+
+  expect(opened_connections).toBe(1);
 });
