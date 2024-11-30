5edd9657d270a5c529c73a131cb7e6daa7f9f8ea
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-11-30 17:31:46 +0000

    use absolute imports to import `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,8 @@
-import database from "../../../../infra/database";
+import database from "/infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
 
   response.status(200).json({
     ola: "mundo",
