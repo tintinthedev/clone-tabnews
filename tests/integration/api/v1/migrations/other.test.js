d23401da31f4378f9c0c00b466db39220ab9939f
Timóteo <152414548+tintinthedev@users.noreply.github.com>
2024-12-17 18:49:10 +0000

    test: make tests more descriptive

--- tests/integration/api/v1/migrations/other.test.js
+++ tests/integration/api/v1/migrations/other.test.js
@@ -4,10 +4,17 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("/api/v1/migrations should return 405 for methods that are not GET or POST", async () => {
-  const putRequest = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
+describe("PUT /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Checking endpoint security", async () => {
+      const putRequest = await fetch(
+        "http://localhost:3000/api/v1/migrations",
+        {
+          method: "PUT",
+        },
+      );
 
-  expect(putRequest.status).toBe(405);
+      expect(putRequest.status).toBe(405);
+    });
+  });
 });
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,26 +6,37 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThanOrEqual(0);
+      });
+
+      test("For the second time", async () => {
+        const migrationsLeftToRun = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        const migrationsLeftToRunResult = await migrationsLeftToRun.json();
+
+        expect(Array.isArray(migrationsLeftToRunResult)).toBe(true);
+        expect(migrationsLeftToRunResult.length).toBe(0);
+      });
+    });
   });
-
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThanOrEqual(0);
-
-  const migrationsLeftToRun = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  const migrationsLeftToRunResult = await migrationsLeftToRun.json();
-
-  expect(Array.isArray(migrationsLeftToRunResult)).toBe(true);
-  expect(migrationsLeftToRunResult.length).toBe(0);
 });
